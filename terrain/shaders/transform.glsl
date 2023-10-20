c18140d821692f7005b0069e8fcf0d43c8bf570d
Iago Pinto <iago.esp@gmail.com>
2023-10-20 15:54:09 -0300

    add and fix

---
+++ terrain/shaders/transform.glsl
@@ -0,0 +1,67 @@
+#version 430 core
+#define M_PI 3.14159265358979323844f
+
+struct InstancedNoise
+{
+  vec3 vertex;
+  float noiseValue;
+};
+out InstancedNoise outValue;
+
+float hash(float n) { return fract(sin(n) * 1e4); }
+float hash(vec2 p) { return fract(1e4 * sin(17.0 * p.x + p.y * 0.1) * (0.1 + abs(sin(p.y * 13.0 + p.x)))); }
+
+float noise(vec3 x) {
+	const vec3 step = vec3(110, 241, 171);
+
+	vec3 i = floor(x);
+	vec3 f = fract(x);
+
+	// For performance, compute the base input to a 1D hash from the integer part of the argument and the
+	// incremental change to the 1D based on the 3D -> 1D wrapping
+    float n = dot(i, step);
+
+	vec3 u = f * f * (3.0 - 2.0 * f);
+	return mix(mix(mix( hash(n + dot(step, vec3(0, 0, 0))), hash(n + dot(step, vec3(1, 0, 0))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 0))), hash(n + dot(step, vec3(1, 1, 0))), u.x), u.y),
+               mix(mix( hash(n + dot(step, vec3(0, 0, 1))), hash(n + dot(step, vec3(1, 0, 1))), u.x),
+                   mix( hash(n + dot(step, vec3(0, 1, 1))), hash(n + dot(step, vec3(1, 1, 1))), u.x), u.y), u.z);
+}
+
+const mat3 m3  = mat3( 0.00,  0.80,  0.60,
+                      -0.80,  0.36, -0.48,
+                      -0.60, -0.48,  0.64 );
+const mat3 m3i = mat3( 0.00, -0.80, -0.60,
+                       0.80,  0.36, -0.48,
+                       0.60, -0.48,  0.64 );
+
+
+float fbm( in vec3 p, int octaves, float gain, float amplitude, float frequency, float size){
+    float f = 0.0;
+    for(int i=0;i<octaves;i++)
+    {
+      f+=noise(p)*frequency;
+      p = m3*p*(2.f+i/100.f);
+      frequency /= 2.f;
+    }
+
+    f *= size;
+
+    return f/amplitude;
+}
+
+uniform float radius;
+uniform float scale=0.f;
+
+in vec3 vertex;
+
+
+
+void main() {
+
+
+  vec3 sphereCoord = mix(vertex, normalize(vertex)*radius, 1)*scale;
+
+  outValue.vertex = sphereCoord;
+  outValue.noiseValue = fbm(sphereCoord,16, 1.f, .93753125f, .5f, 1);
+}
